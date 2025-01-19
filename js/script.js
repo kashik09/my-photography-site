c3b2819ef3e9d4775ccd76b70f1abaa77181cb1f
Kashi Kweyu <kashiku789@gmail.com>
2025-01-19 22:17:16 +0300

    Added collapse menu script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// JavaScript for menu toggle
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
